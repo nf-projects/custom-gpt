23d79133a2daaa6d0d9bfc79e61c917779eba3eb
nf-projects <90344282+nf-projects@users.noreply.github.com>
2022-12-30 12:54:15 -0800

    force dark mode

--- util/theme.ts
+++ util/theme.ts
@@ -1,8 +1,8 @@
 import {extendTheme} from '@chakra-ui/react'
 
 const theme = extendTheme({
-	initialColorMode: 'system',
-	useSystemColorMode: false,
+	initialColorMode: 'dark',
+	// useSystemColorMode: false,
 	fontFamilies: {
 		body: `'Raleway', sans-serif`,
 		heading: `'Open Sans', sans-serif`,
