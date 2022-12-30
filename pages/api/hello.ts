98759b4119ee860b55d8c5d27994878108e6c5f6
nf-projects <90344282+nf-projects@users.noreply.github.com>
2022-12-30 00:39:58 -0800

    working POC openai api route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const {Configuration, OpenAIApi} = require("openai");
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Hello world, what is your name?",
+  });
+  res.status(200).json({"output": completion.data.choices[0].text})
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
+import {Input} from "@chakra-ui/input";
+import {Button, Container, VStack} from "@chakra-ui/react";
+import {useState} from "react";
+// const { Configuration, OpenAIApi } = require("openai");
+//
+// const configuration = new Configuration({
+// 	apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+
 export default function Home() {
+	const [input, setInput] = useState("");
+
+	async function handleSubmit() {
+		const data = await fetch("/api/hello");
+		const response = await data.json();
+		console.log(response);
+	}
+
 	return (
 		<>
-			<div>Hello World</div>
+			<Container p={25}>
+				<VStack>
+					<Input placeholder="Enter Input..."
+					       onChange={(e) => setInput(e.target.value)}
+					       value={input}></Input>
+					<Button onClick={handleSubmit}>Submit</Button>
+				</VStack>
+
+			</Container>
+
 		</>
 	)
 }
