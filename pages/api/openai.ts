106c95d867f0474a88f5912f1ca5797f006ed64c
nf-projects <90344282+nf-projects@users.noreply.github.com>
2023-01-02 13:16:14 -0800

    temp disable api

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -2,23 +2,24 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const {Configuration, OpenAIApi} = require("openai");
+	// const {Configuration, OpenAIApi} = require("openai");
 
-	const configuration = new Configuration({
-		apiKey: process.env.OPENAI_API_KEY,
-	});
-	const openai = new OpenAIApi(configuration);
+	// const configuration = new Configuration({
+	// 	apiKey: process.env.OPENAI_API_KEY,
+	// });
+	// const openai = new OpenAIApi(configuration);
 
-	console.log(req.body.prompt);
+	// console.log(req.body.prompt);
 
-	const completion = await openai.createCompletion({
-		model: req.body.model,
-		prompt: req.body.prompt,
-		max_tokens: req.body.max_tokens,
-		temperature: req.body.temperature,
-		top_p: req.body.top_p,
-		frequency_penalty: req.body.frequency_penalty,
-		presence_penalty: req.body.presence_penalty,
-	})
-	res.status(200).json(completion.data.choices[0].text)
+	// const completion = await openai.createCompletion({
+	// 	model: req.body.model,
+	// 	prompt: req.body.prompt,
+	// 	max_tokens: req.body.max_tokens,
+	// 	temperature: req.body.temperature,
+	// 	top_p: req.body.top_p,
+	// 	frequency_penalty: req.body.frequency_penalty,
+	// 	presence_penalty: req.body.presence_penalty,
+	// })
+	// res.status(200).json(completion.data.choices[0].text)
+	res.status(200).json({"error": "api is temp disabled"})
 }
